b3144bfa265581df642c9f8bf6430773adb247bb
franot.kim-celine <kimceline.franot@gmail.com>
2022-10-15 23:34:20 +0200

    new component (not implemented)

---
+++ mini-project-angular/src/app/order-box/order-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-box',
+  templateUrl: './order-box.component.html',
+  styleUrls: ['./order-box.component.css']
+})
+export class OrderBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
