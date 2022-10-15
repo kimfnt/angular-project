1c36cedbba63f5a79e00d98d4fc031fa0f05e606
franot.kim-celine <kimceline.franot@gmail.com>
2022-10-15 23:35:12 +0200

    new design of list of products

--- mini-project-angular/src/app/products-list/products-list.component.ts
+++ mini-project-angular/src/app/products-list/products-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-products-list',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsListComponent implements OnInit {
 
-  constructor() { }
+  selectedProduct?: Product;
+
+  products: Array<Product> | undefined;
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  loadProducts(): void {
+    this.productService.getProducts().subscribe((_products: any) => {
+      this.products = _products.sort((a: any, b: any) => {
+        if (a.name < b.name) return -1;
+        if (a.name > b.name) return 1;
+        return 0;
+      });
+    })
+  }
+
+  onSelect(product: Product): void {
+    this.selectedProduct = product;
   }
 
 }
